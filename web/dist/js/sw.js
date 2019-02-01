2fe121d4188df80f4a15c80713bc40d855a96c9a
Dustin <dustin1114@gmail.com>
2019-02-01 17:03:19 -0500

    Improve service-worker caching (#23)
    This commit makes the PWA service worker caching more aggressive, while
    also adding proper cleanup for old cache data. Instead of only caching
    static resources (JS, CSS, images, etc.), we should also be caching page
    requests. This is done with each request attempting to look in the cache
    first, and if not found, fetch from the network (then cache). The
    rational can be found here:
    https://developer.mozilla.org/en-US/docs/Web/Apps/Progressive/Offline_Service_workers

--- web/dist/js/sw.js
+++ web/dist/js/sw.js
@@ -1,14 +1,30 @@
-// version: 2 (improve logos)
+const CACHE_NAME = "biblers-cache-v3";
 
+// Use cache before fetching from the network.
 self.addEventListener("fetch", (e) => {
-    e.respondWith(
-        caches.match(e.request).then(resp => resp || fetch(e.request)),
-    );
+    // Search API requests are not locally cached.
+    if (e.request.url.indexOf("/api/search") > -1) {
+        e.respondWith(fetch(e.request));
+
+    // Standard pages are cached.
+    } else {
+        e.respondWith(
+            caches.match(e.request).then(initialResp =>
+                initialResp || fetch(e.request).then(resp =>
+                    caches.open(CACHE_NAME).then(cache => {
+                        cache.put(e.request, resp.clone());
+                        return resp;
+                    })
+                )
+            ),
+        )
+    }
 });
 
+// Install static files.
 self.addEventListener("install", (e) => {
     e.waitUntil(
-        caches.open("biblers-cache").then(cache => cache.addAll([
+        caches.open(CACHE_NAME).then(cache => cache.addAll([
             "/",
             "/static/manifest.json",
             "/static/css/style.css",
@@ -21,5 +37,18 @@ self.addEventListener("install", (e) => {
             "/static/js/autocomplete.min.js",
             "/static/js/main.js",
         ])),
+    );
+});
+
+// Clear the old caches.
+self.addEventListener("activate", (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) =>
+            Promise.all(keyList.map((k) => {
+                if (CACHE_NAME.indexOf(k) === -1) {
+                    return caches.delete(k);
+                }
+            }))
+        ),
     );
 });
