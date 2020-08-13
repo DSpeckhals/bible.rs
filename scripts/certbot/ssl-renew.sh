3f1934aca3566f0f1cf929b14713e2284b47c3e7
Dustin Speckhals <dustin1114@gmail.com>
2020-08-13 17:51:24 -0400

    Test Kubernetes

--- scripts/certbot/ssl-renew.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cd "/home/$USER/bible.rs"
-
-docker-compose run --rm certbot renew && docker-compose kill -s SIGHUP gateway
--- scripts/deploy/remote-deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cd "/home/$USER/bible.rs"
-
-docker-compose pull
-
-sudo systemctl restart biblers.service
